18079a0a344a2a140b5036bbe82b37938849e7ec
dideey <bradleyjd32@gmail.com>
2023-11-24 02:09:46 +0300

    update on filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,12 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is not None:
+            return [obj for obj in FileStorage.__objects.values() if isinstance(obj, cls)]
+        else:
+            return list(FileStorage.__objects.values())
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +27,15 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+        #Added instance
+    def delete(self, obj=None):
+        if obj is not None:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            del FileStorage.__objects[key]
+
+
+
 
     def reload(self):
         """Loads storage dictionary from file"""
