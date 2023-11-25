350187de91922ba964cf9b9a8f1eb85015e0f6b7
Aymen Idouahman <aymanidou@gmail.com>
2023-11-25 22:52:27 +0100

    update console

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,6 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
-from models import storage
 
 Base = declarative_base()
 
@@ -37,6 +36,7 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
+        from models import storage
         self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
@@ -57,4 +57,5 @@ class BaseModel:
 
     def delete(self):
         """delete the current instance from the storage"""
+        from models import storage
         storage.delete(self)
