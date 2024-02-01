b3a1716bbe4590697e6c46358d42a4170f535b21
dideey <bradleyjd32@gmail.com>
2024-02-02 02:52:51 +0300

    deployment

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,24 +8,24 @@ env.hosts = ['	54.237.80.147', '100.24.235.237']
 
 
 def do_deploy(archive_path):
-    """deploys web_static
+    """ distributes an archive to your web servers
     """
     if exists(archive_path) is False:
         return False
-        archive_filename = archive_path.split('/')[-1]
-        archive_folder = '/data/web_static/releases/{}'.format(
-            archive_filename.split('.')[0])
+    file = archive_path.split('/')[-1]
+    extract_tgz = '/data/web_static/releases/'\
+        + "{}".format(file.split('.')[0])
+    tmp = "/tmp/" + file
+
     try:
         put(archive_path, "/tmp/")
-        run('sudo mkdir -p {}./'.format(archive_folder))
-        run('sudo tar -xzf /tmp/{} -C {}/'.format(
-            archive_filename, archive_folder))
-        run('sudo rm /tmp/{}'.format(archive_filename))
-        run("sudo mv {}/web_static/* {}/".format(
-            archive_folder, archive_folder))
-        run("sudo rm -rf {}/web_static".format(archive_folder))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {} /data/web_static/current'.format(archive_folder))
+        run("sudo mkdir -p {}/".format(extract_tgz))
+        run("sudo tar -xzf {} -C {}/".format(tmp, extract_tgz))
+        run("sudo rm {}".format(tmp))
+        run("sudo mv {}/web_static/* {}/".format(extract_tgz, extract_tgz))
+        run("sudo rm -rf {}/web_static".format(extract_tgz))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {}/ /data/web_static/current".format(extract_tgz))
         return True
     except Exception:
         return False
