af083af5eccb35f1539b034a6e37801e1d58a068
dideey <bradleyjd32@gmail.com>
2024-02-02 02:42:04 +0300

    deployment

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,20 +11,21 @@ def do_deploy(archive_path):
     """deploys web_static
     """
     if exists(archive_path) is False:
-        return False'
+        return False
         archive_filename = archive_path.split('/')[-1]
         archive_folder = '/data/web_static/releases/{}'.format(
             archive_filename.split('.')[0])
     try:
         put(archive_path, "/tmp/")
         run('sudo mkdir -p {}./'format(archive_folder))
-        run('sudo tar -xzf /tmp/{} -C {}/'.format(archive_filename, archive_folder))
+        run('sudo tar -xzf /tmp/{} -C {}/'.format(
+            archive_filename, archive_folder))
         run('sudo rm /tmp/{}'.format(archive_filename))
-        run("sudo mv {}/web_static/* {}/".format(archive_folder, archive_folder))
+        run("sudo mv {}/web_static/* {}/".format(
+            archive_folder, archive_folder))
         run("sudo rm -rf {}/web_static".format(archive_folder))
         run('sudo rm -rf /data/web_static/current')
         run('sudo ln -s {} /data/web_static/current'.format(archive_folder))
         return True
     except Exception:
         return False
-
