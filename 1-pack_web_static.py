fe53bc74d2126732d5970baf923752bc62d27c57
dideey <bradleyjd32@gmail.com>
2024-02-01 18:26:24 +0300

    pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+
 """A Fabric script that generates a .tgz archive
    from the contents of the web_static folder
 """
 from fabric.api import *
 from datetime import datetime
+
+
 def do_pack():
+
     """"Creates the .tgz files"""
 
     local("sudo mkdir -p versions")
